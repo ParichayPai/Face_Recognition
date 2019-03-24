7788dadb425d47ea5b05c58d7e4180793735fb4c
ParichayPai <39957257+ParichayPai@users.noreply.github.com>
2019-03-24 09:32:23 +0530

    Update OpenCV_Face_recog.py

--- OpenCV_Face_recog.py
+++ OpenCV_Face_recog.py
@@ -23,10 +23,13 @@ def detect(gray, frame):
 video_capture = cv2.VideoCapture(0)
 while True:
     _, frame = video_capture.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    canvas = detect(gray, frame)
-    cv2.imshow('Video', canvas)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    if _ is True:
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        canvas = detect(gray, frame)
+        cv2.imshow('Video', canvas)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        continue
 video_capture.release()
 cv2.destroyAllWindows()
